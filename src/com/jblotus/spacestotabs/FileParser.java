9da03e5c1c6497336cc9f9690da141571da33504
jblotus <jblotus@gmail.com>
2010-12-15 17:46:20 -0500

    added basic replace method

--- src/com/jblotus/spacestotabs/FileParser.java
+++ src/com/jblotus/spacestotabs/FileParser.java
@@ -1,5 +1,48 @@
 package com.jblotus.spacestotabs;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class FileParser {
+
+	public static StringBuffer parse(File file) {
+		// TODO Auto-generated method stub
+		System.out.println(file.getAbsolutePath());
+		
+		String inputString = ""; 
+		//need to return a new file that has been parsed
+		StringBuffer newFileContent = new StringBuffer("");
+		
+		Pattern p = Pattern.compile("^\\s+");	
+		try {
+				BufferedReader in = new BufferedReader(new FileReader(file));			
+				String replaceString = null;
+				while((inputString = in.readLine()) != null) {	
+					
+					Matcher m = p.matcher(inputString);
+					boolean result= m.find();
+					System.out.println(result);
+					/**
+					 * If we get a match on our regex test replace this string
+					 */
+					if (result) {
+						newFileContent.append(m.replaceAll(" "));
+					} else {
+						newFileContent.append(inputString);
+					}
+				}
+			} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return newFileContent;    
+			
+	}
 
 }
--- src/com/jblotus/spacestotabs/MainGUI.java
+++ src/com/jblotus/spacestotabs/MainGUI.java
@@ -1,13 +1,19 @@
 package com.jblotus.spacestotabs;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class MainGUI extends JPanel {	
 	
@@ -24,16 +30,48 @@ public class MainGUI extends JPanel {
 		//load frame
 		JFrame frame = new JFrame();
 		frame.setSize(400, 300);
+		frame.setTitle("SpacesToTabsParser");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//add in the menu
 		JMenuBar menuBar = new JMenuBar();
 		JMenu filemenu = new JMenu("File");
-		JMenuItem open = new JMenuItem("Open");	
+		final JMenuItem open = new JMenuItem("Open");	
+		
+
 		
 		filemenu.add(open);
 		menuBar.add(filemenu);
 		//add the menu bar
 		frame.add(BorderLayout.NORTH, menuBar);
+		
+		//two panels to diff results
+		JTextArea originalFile = new JTextArea("original");
+		JTextArea newFile = new JTextArea("new");
+		frame.add(BorderLayout.CENTER, originalFile);
+		frame.add(BorderLayout.EAST, newFile);
+		
+		open.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fc = new JFileChooser();
+			    
+				int returnVal = fc.showOpenDialog(open);
+				 if (returnVal == JFileChooser.APPROVE_OPTION) {
+			            File file = fc.getSelectedFile();
+			            //This is where a real application would open the file.
+			            System.out.println(file.getName());
+			            
+			            //now we need to parse the file
+			            FileParser fp = new FileParser();
+			            StringBuffer newFileContent = FileParser.parse(file);
+			            System.out.println(newFileContent);
+			        } else {
+			        	System.out.println("error");
+			        }
+			}
+		});		
 		frame.setVisible(true);
 	}
 
